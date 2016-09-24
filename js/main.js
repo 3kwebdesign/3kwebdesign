1e35675d26e717718da27b0b4d01269878e4d72f
3kwebdesign <my@3kwebdesign.com>
2016-09-24 11:33:15 +0200

    Newest version

--- js/main.js
+++ js/main.js
@@ -3,13 +3,38 @@ $(document).ready(function()
 	Web.init();
 	Web.Menu.init();
 	Web.Contact.init();
+	Input.init();
 });
+
+var cfg = {
+	transitionEnd: 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+};
 
 Web = {
 	init: function()
 	{
 		this.page = $('#page');
 		this.parts = $('#page .part');
+	},
+
+	getTransitionProperty(element) 
+	{
+	  // Note that in some versions of IE9 it is critical that
+	  // msTransform appear in this list before MozTransform
+	  var properties = [
+	    'transition',
+	    'WebkitTransition',
+	    'msTransition',
+	    'MozTransition',
+	    'OTransition'
+	  ];
+	  var p;
+	  while (p = properties.shift()) {
+	    if (typeof element.style[p] != 'undefined') {
+	      return p;
+	    }
+	  }
+	  return false;
 	}
 };
 
@@ -20,19 +45,21 @@ Web.Menu = {
 		this.header = $('#header');
 		this.primary_nav_items = $('#header .right .primary-nav a');
 		this.slick_btn = $('#header .right .slick-nav .slick-btn');
+		this.slick_menu = $('#slick-menu');
 		this.slick_menu_items = $('#slick-menu .content .menu a');
 
 		this.toggleHeader();
 		self.toggleNavItemWhenScrolled();
 
-		$('a[href*=#]:not([href=#])').click(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
+			if (target.length)
+			{
 				$('html,body').animate({
 					scrollTop: target.offset().top
-				}, 800);
+				}, 500);
 				return false;
 			}
 		}
@@ -127,6 +154,20 @@ Web.Menu = {
 	toggleSlickMenu: function()
 	{
 		Web.page.toggleClass('slick_menu_off slick_menu_on');
+		this.animateSlickBtn();
+	},
+
+	animateSlickBtn: function()
+	{
+		var _this = this.slick_btn;
+		
+		_this.addClass('scale');
+		var duration = parseFloat(_this.css(Web.getTransitionProperty(_this[0]) + '-duration')) * 1000;
+		window.setTimeout(function()
+		{
+			_this.children().first().toggleClass('fa-bars fa-times');
+			_this.removeClass('scale');
+		}, duration);
 	}
 };
 
@@ -144,5 +185,70 @@ Web.Contact = {
 			}
 			});
 		});
+	}
+};
+
+var Input = {
+	xDown: null,                                                     
+	yDown: null,
+	sLeft: false,
+	sRight: false,
+	sUp: false,
+	sDown: false,
+
+	init:function()
+	{
+		var handleTouchStart = function(e)
+		{                                         
+		    this.xDown = e.touches[0].clientX;                                      
+		    this.yDown = e.touches[0].clientY;                                      
+		};                                            
+
+		var handleTouchMove = function(e)
+		{
+		    if (!this.xDown || !this.yDown) return;
+
+		    var xUp = e.touches[0].clientX;                                    
+		    var yUp = e.touches[0].clientY;
+
+		    var xDiff = xDown - xUp;
+		    var yDiff = yDown - yUp;
+
+		    if (Math.abs(xDiff) > Math.abs(yDiff))
+		    {
+		        if ( xDiff > 0 )
+		        {
+		            /* left swipe */
+		            if(Web.page.hasClass('slick_menu_off'))
+		            {
+		            	Web.Menu.toggleSlickMenu();
+		            }
+		        }
+		        else
+		        {
+		            /* right swipe */
+		            if(Web.page.hasClass('slick_menu_on'))
+		            {
+		            	Web.Menu.toggleSlickMenu();
+		            }
+		        }                       
+		    }
+		    else
+		    {
+		        if ( yDiff > 0 )
+		        {
+		            /* up swipe */ 
+		        }
+		        else { 
+		            /* down swipe */
+		        }                                                                 
+		    }
+
+		    xDown = null;
+		    yDown = null;                                             
+		};
+
+		document.addEventListener('touchstart', handleTouchStart, false);        
+		document.addEventListener('touchmove', handleTouchMove, false);
 	}
 };
